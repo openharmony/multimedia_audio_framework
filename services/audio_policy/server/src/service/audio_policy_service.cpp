c2507696fd30b3fb1ff3e350856d3239b3e947ee
liyuhang <liyuhang24@huawei.com>
2023-09-20 15:34:35 +0800

    fixed 3471c56 from https://gitee.com/imyuhang/multimedia_audio_framework/pulls/1979 Add shared lock to prevent race condition
    Signed-off-by: liyuhang <liyuhang24@huawei.com>
    Change-Id: I04f63c64709f87276dbded5e81af800915de21d6

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -605,6 +605,8 @@ int32_t AudioPolicyService::OpenRemoteAudioDevice(std::string networkId, DeviceR
     auto isPresent = [&deviceType, &networkId] (const sptr<AudioDeviceDescriptor> &descriptor) {
         return descriptor->deviceType_ == deviceType && descriptor->networkId_ == networkId;
     };
+
+    std::lock_guard<std::shared_mutex> lock(deviceStatusUpdateSharedMutex_);
     connectedDevices_.erase(std::remove_if(connectedDevices_.begin(), connectedDevices_.end(), isPresent),
         connectedDevices_.end());
     UpdateDisplayName(remoteDeviceDescriptor);
@@ -1607,6 +1609,8 @@ void AudioPolicyService::SetEarpieceState()
             CHECK_AND_RETURN_RET_LOG(desc != nullptr, false, "Invalid device descriptor");
             return desc->deviceType_ == DEVICE_TYPE_EARPIECE;
         };
+
+        std::lock_guard<std::shared_mutex> lock(deviceStatusUpdateSharedMutex_);
         auto itr = std::find_if(connectedDevices_.begin(), connectedDevices_.end(), isPresent);
         if (itr != connectedDevices_.end()) {
             connectedDevices_.erase(itr);
