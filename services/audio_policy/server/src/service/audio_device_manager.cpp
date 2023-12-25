2cf43b8ffd7a6be2501dabdbc9ac7c4427c6d881
huyue57 <huyue57@huawei.com>
2023-12-25 17:30:02 +0800

    fixed b3c3067 from https://gitee.com/huyue57/multimedia_audio_framework/pulls/2526 修复先连接的佩戴检测耳机，后佩戴，不从该耳机发声
    Signed-off-by: huyue57 <huyue57@huawei.com>
    Change-Id: I076e18476c0c90faa8d01a747ce83b924a67f968

--- services/audio_policy/server/src/service/audio_device_manager.cpp
+++ services/audio_policy/server/src/service/audio_device_manager.cpp
@@ -681,6 +681,8 @@ void AudioDeviceManager::UpdateDeviceCategory(const sptr<AudioDeviceDescriptor> 
             if (devDesc->deviceCategory_ == BT_UNWEAR_HEADPHONE) {
                 RemoveBtFromOtherList(deviceDescriptor);
             } else {
+                // Update connectTimeStamp_ when wearing headphones that support wear detection
+                desc->connectTimeStamp_ = GetCurrentTimeMS();
                 AddBtToOtherList(desc);
             }
         }
