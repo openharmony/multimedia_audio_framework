52d7b390c0925e3e6ebf0919831eb6880d54064f
liyuhang <liyuhang24@huawei.com>
2024-01-30 20:50:53 +0800

    fixed ce0fd16 from https://gitee.com/imyuhang/multimedia_audio_framework/pulls/2806 Remove unserviceable code
    Signed-off-by: liyuhang <liyuhang24@huawei.com>
    Change-Id: If2840a3b1b1b9e3990febe33b2ce93b26d197a2f

--- services/audio_policy/client/src/audio_policy_manager.cpp
+++ services/audio_policy/client/src/audio_policy_manager.cpp
@@ -125,7 +125,6 @@ void AudioPolicyManager::AudioPolicyServerDied(pid_t pid)
             cb = it->second.lock();
             if (cb != nullptr) {
                 cb->OnAudioPolicyServiceDied();
-                rendererCBMap_.erase(getpid());
             }
         }
     }
