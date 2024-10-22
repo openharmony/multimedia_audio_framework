a2783dfc55f50581eca5c35fb117da5d0dcea084
yjguo <guoyanjun10@huawei.com>
2024-10-22 10:49:29 +0800

    add0903
    Signed-off-by: yjguo <guoyanjun10@huawei.com>

--- interfaces/inner_api/native/audiocommon/include/audio_info.h
+++ interfaces/inner_api/native/audiocommon/include/audio_info.h
@@ -433,6 +433,7 @@ struct AudioRendererOptions {
     AudioStreamInfo streamInfo;
     AudioRendererInfo rendererInfo;
     AudioPrivacyType privacyType = PRIVACY_TYPE_PUBLIC;
+    AudioSessionStrategy strategy = { AudioConcurrencyMode::INVALID };
 };
 
 struct MicStateChangeEvent {
